5a3862c2fbc0b194e815e389c12a0d630a6ceb5a
Nghiemthohieu <hieunghiem2712@gmail.com>
2025-05-16 10:06:58 +0000

    feat: upImage productcolor

--- src/routers/manager/product_color/index.js
+++ src/routers/manager/product_color/index.js
@@ -4,13 +4,20 @@ const express = require('express');
 const { asyncHandler } = require('../../../auth/checkAuth');
 const product_colorController = require('../../../controllers/product_color.controller');
 const { authentication } = require('../../../auth/authUtils');
+const multer = require('multer');
+const storage = multer.memoryStorage(); // Để upload lên S3 nhanh hơn
+const upload = multer({ storage });
 const router = express.Router();
 
 // router.use(authentication);
-router.post('/create', asyncHandler(product_colorController.CreateProductColor))
+router.post('/create', upload.fields([
+    { name: 'color_image', maxCount: 1 },
+]), asyncHandler(product_colorController.CreateProductColor))
 router.get('/detail/:id', asyncHandler(product_colorController.GetByProductColorId))
 router.get('/list', asyncHandler(product_colorController.GetAllProductColor))
-router.put('/update', asyncHandler(product_colorController.UpdateProductColor))
+router.put('/updat/:id', upload.fields([
+    { name: 'color_image', maxCount: 1 },
+]), asyncHandler(product_colorController.UpdateProductColor))
 router.delete('/delete/:id', asyncHandler(product_colorController.DeleteProductColor))
 
 module.exports = router;
--- src/services/product_color.service.js
+++ src/services/product_color.service.js
@@ -10,31 +10,79 @@ const { uploadFileToS3 } = require("../utils/uploadFileToS3");
 class ProductColorService {
 
     static DeleteProductColor = async (id) => {
-        return {data: await product_colorModel.findByIdAndDelete(id)};
+        return { data: await product_colorModel.findByIdAndDelete(id) };
     }
 
-    static UpdateProductColor = async (data) => {
-        if (!data._id) {
-            logger.error('Vui lòng điền _id của size');
-            throw new notFoundError('Vui lòng điền _id của size');
+    static UpdateProductColor = async (colorId, files, data) => {
+        const color = await product_colorModel.findById(colorId);
+        if (!color) {
+            logger.error("Color not found");
+            throw new NotFoundError("Color not found");
         }
-        let colorImage = decodeBase64Image(data.colorImage);
-        data.colorImage = await uploadFileToS3(colorImage);
-        return {data: await product_colorModel.findByIdAndUpdate(data._id, data, {new: true})};
-    }
-    
-    static createProductColor = async (data) => {
-        let colorImage = decodeBase64Image(data.colorImage);
-        data.colorImage = await uploadFileToS3(colorImage);
-        return {data: await product_colorModel.create(data)};
-    }
+
+        let uploadedImage = color.colorImage; // giữ ảnh cũ nếu không có ảnh mới
+
+        const color_image = files['color_image'] || [];
+
+        if (color_image.length > 0) {
+            uploadedImage = await uploadFileToS3(
+                color_image[0].buffer,
+                color_image[0].originalname,
+                color_image[0].mimetype
+            );
+        }
+
+        const updatedData = {
+            ...data,
+            colorImage: uploadedImage
+        };
+
+        const updatedColor = await product_colorModel.findByIdAndUpdate(
+            colorId,
+            updatedData,
+            { new: true }
+        );
+
+        return { data: updatedColor };
+    };
+
+    static createProductColor = async (files, metaData) => {
+        try {
+            // Lấy file ảnh từ field "color_image"
+            const colorImageFiles = files['color_image'] || [];
+
+            // Kiểm tra nếu không có file
+            if (colorImageFiles.length === 0) {
+                throw new BadRequestError('Không có ảnh màu được upload');
+            }
+
+            // Upload ảnh đầu tiên (vì chỉ có 1 ảnh màu)
+            const uploadedImage = await uploadFileToS3(
+                colorImageFiles[0].buffer,
+                colorImageFiles[0].originalname,
+                colorImageFiles[0].mimetype
+            );
+
+            // Tạo dữ liệu lưu vào MongoDB
+            const data = {
+                ...metaData,
+                colorImage: uploadedImage
+            };
+
+            const createdColor = await product_colorModel.create(data);
+            return { data: createdColor };
+
+        } catch (err) {
+            throw new Error(`Tạo color thất bại: ${err.message}`);
+        }
+    };
 
     static GetByProductColorId = async (id) => {
-        return {data: await product_colorModel.findById(id)};
+        return { data: await product_colorModel.findById(id) };
     }
 
     static GetAllProductColor = async (query) => {
-        return await paginate(product_colorModel, {}, {page: query.page, limit: query.limit});
+        return await paginate(product_colorModel, {}, { page: query.page, limit: query.limit });
     }
 }
 
